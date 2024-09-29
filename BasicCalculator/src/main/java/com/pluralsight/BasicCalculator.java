cd9bf4227942d655324ec9c891eb2599677affbe
Elan Sahalon <Esahalon1996@gmail.com>
2024-09-28 20:04:18 -0400

    Calucaltor demo

--- BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
+++ BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
@@ -1,4 +1,42 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class BasicCalculator {
+    public static void main (String[] args ){
+       Scanner scanner = new Scanner(System.in);
+       // Two float numbers
+        System.out.println(" Enter the first number: ");
+        Float num1 = scanner.nextFloat();
+
+        System.out.println(" Enter the Second number ");
+        Float num2 = scanner.nextFloat();
+
+        // Possible outcomes
+        System.out.println(" Enter one of the 4 operations: 1.Add, 2.Subtract, 3.Mulitply, 4.Divide ");
+        int choice = scanner.nextInt();
+
+        if (choice== 1) {
+            System.out.println("Result: " + (num1 + num2));
+        }
+        if (choice== 2) {
+            System.out.println("Result: " + (num1 - num2));
+        }
+        if (choice== 3) {
+            System.out.println("Result: " + (num1 * num2));
+        }
+        if (choice== 4) {
+            System.out.println("Result: " + (num1 / num2));
+        }
+        scanner.close();
+
+
+        }
+
+
+
+
 }
+
+
+
